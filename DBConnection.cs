3c257a8b0f125f38937b31ef9a86b907d349b84e
ky-9 <kylamaee09@gmail.com>
2026-06-06 22:03:43 +0800

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace HRSystem
+{
+    public class DBConnection
+    {
+        private MySqlConnection conn;
+
+        public DBConnection()
+        {
+            conn = new MySqlConnection(
+                "server=localhost;port=3306;database=hr_db;uid=root;pwd=KylaM@e123;"
+            );
+        }
+
+        public void Open()
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+        }
+
+        public void Close()
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return conn;
+        }
+    }
+}
