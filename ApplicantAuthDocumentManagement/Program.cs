82c514eb3d3f04d681966053b62a0ec4713315e1
martinez-marithe <marithecm1028@gmail.com>
2026-06-07 22:09:23 +0800

    updated program.cs

--- ApplicantAuthDocumentManagement/Program.cs
+++ ApplicantAuthDocumentManagement/Program.cs
@@ -1,20 +1,12 @@
 ﻿using ApplicantAuthDocumentManagement.Forms;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ApplicantAuthDocumentManagement
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-
-       
         static void Main()
         {
             Application.EnableVisualStyles();
