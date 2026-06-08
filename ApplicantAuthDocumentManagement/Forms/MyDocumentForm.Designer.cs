eb74fcaa9e277bb9b3a54f64f8d5415c84dd4937
martinez-marithe <marithecm1028@gmail.com>
2026-06-08 23:26:44 +0800

    Develop my documents module for upload and track requirements

--- ApplicantAuthDocumentManagement/Forms/MyDocumentForm.Designer.cs
+++ ApplicantAuthDocumentManagement/Forms/MyDocumentForm.Designer.cs
@@ -28,12 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.pnlBackground = new System.Windows.Forms.Panel();
+            this.grpTimeline = new System.Windows.Forms.GroupBox();
+            this.dgvTimeline = new System.Windows.Forms.DataGridView();
+            this.grpDocuments = new System.Windows.Forms.GroupBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.lblMissingIndicator = new System.Windows.Forms.Label();
+            this.lblSubmittedIndicator = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.pnlBackground.SuspendLayout();
+            this.grpTimeline.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimeline)).BeginInit();
+            this.grpDocuments.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.Thistle;
+            this.pnlHeader.Controls.Add(this.lblCompany);
+            this.pnlHeader.Controls.Add(this.picLogo);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(900, 70);
+            this.pnlHeader.TabIndex = 8;
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Font = new System.Drawing.Font("Lucida Fax", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompany.ForeColor = System.Drawing.Color.White;
+            this.lblCompany.Location = new System.Drawing.Point(20, 18);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(343, 32);
+            this.lblCompany.TabIndex = 0;
+            this.lblCompany.Text = "Apex Digital Solutions";
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackColor = System.Drawing.SystemColors.Control;
+            this.picLogo.Image = global::ApplicantAuthDocumentManagement.Properties.Resources.logo2;
+            this.picLogo.Location = new System.Drawing.Point(-14, -49);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(0, 0);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.BackColor = System.Drawing.Color.LavenderBlush;
+            this.pnlBackground.Controls.Add(this.grpTimeline);
+            this.pnlBackground.Controls.Add(this.grpDocuments);
+            this.pnlBackground.Controls.Add(this.lblWelcome);
+            this.pnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBackground.Location = new System.Drawing.Point(0, 70);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(900, 520);
+            this.pnlBackground.TabIndex = 9;
+            // 
+            // grpTimeline
+            // 
+            this.grpTimeline.Controls.Add(this.dgvTimeline);
+            this.grpTimeline.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpTimeline.ForeColor = System.Drawing.Color.DimGray;
+            this.grpTimeline.Location = new System.Drawing.Point(25, 365);
+            this.grpTimeline.Name = "grpTimeline";
+            this.grpTimeline.Size = new System.Drawing.Size(850, 200);
+            this.grpTimeline.TabIndex = 2;
+            this.grpTimeline.TabStop = false;
+            this.grpTimeline.Text = "Application Status Timeline & Visible HR Remarks";
+            // 
+            // dgvTimeline
+            // 
+            this.dgvTimeline.BackgroundColor = System.Drawing.Color.White;
+            this.dgvTimeline.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTimeline.Location = new System.Drawing.Point(20, 35);
+            this.dgvTimeline.Name = "dgvTimeline";
+            this.dgvTimeline.ReadOnly = true;
+            this.dgvTimeline.RowHeadersWidth = 51;
+            this.dgvTimeline.RowTemplate.Height = 24;
+            this.dgvTimeline.Size = new System.Drawing.Size(810, 140);
+            this.dgvTimeline.TabIndex = 0;
+            // 
+            // grpDocuments
+            // 
+            this.grpDocuments.Controls.Add(this.btnUpload);
+            this.grpDocuments.Controls.Add(this.dgvDocuments);
+            this.grpDocuments.Controls.Add(this.lblMissingIndicator);
+            this.grpDocuments.Controls.Add(this.lblSubmittedIndicator);
+            this.grpDocuments.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpDocuments.ForeColor = System.Drawing.Color.DimGray;
+            this.grpDocuments.Location = new System.Drawing.Point(25, 90);
+            this.grpDocuments.Name = "grpDocuments";
+            this.grpDocuments.Size = new System.Drawing.Size(850, 260);
+            this.grpDocuments.TabIndex = 1;
+            this.grpDocuments.TabStop = false;
+            this.grpDocuments.Text = "My Documents Module (Upload & Tracking)";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.Thistle;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpload.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(20, 210);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(250, 35);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Select and Upload Document File";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.BackgroundColor = System.Drawing.Color.White;
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(20, 65);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.ReadOnly = true;
+            this.dgvDocuments.RowHeadersWidth = 51;
+            this.dgvDocuments.RowTemplate.Height = 24;
+            this.dgvDocuments.Size = new System.Drawing.Size(810, 130);
+            this.dgvDocuments.TabIndex = 2;
+            // 
+            // lblMissingIndicator
+            // 
+            this.lblMissingIndicator.AutoSize = true;
+            this.lblMissingIndicator.ForeColor = System.Drawing.Color.Red;
+            this.lblMissingIndicator.Location = new System.Drawing.Point(250, 28);
+            this.lblMissingIndicator.Name = "lblMissingIndicator";
+            this.lblMissingIndicator.Size = new System.Drawing.Size(124, 23);
+            this.lblMissingIndicator.TabIndex = 1;
+            this.lblMissingIndicator.Text = "Missing: 0 slots";
+            // 
+            // lblSubmittedIndicator
+            // 
+            this.lblSubmittedIndicator.AutoSize = true;
+            this.lblSubmittedIndicator.ForeColor = System.Drawing.Color.Green;
+            this.lblSubmittedIndicator.Location = new System.Drawing.Point(20, 28);
+            this.lblSubmittedIndicator.Name = "lblSubmittedIndicator";
+            this.lblSubmittedIndicator.Size = new System.Drawing.Size(146, 23);
+            this.lblSubmittedIndicator.TabIndex = 0;
+            this.lblSubmittedIndicator.Text = "Submitted: 0 slots";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.DimGray;
+            this.lblWelcome.Location = new System.Drawing.Point(550, 25);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(178, 23);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Applicant Workstation";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MyDocumentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MyDocumentForm";
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(900, 590);
+            this.Controls.Add(this.pnlBackground);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MyDocumentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Applicant Attachment Requirements & Progress Tracker";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.pnlBackground.ResumeLayout(false);
+            this.pnlBackground.PerformLayout();
+            this.grpTimeline.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimeline)).EndInit();
+            this.grpDocuments.ResumeLayout(false);
+            this.grpDocuments.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Panel pnlBackground;
+        private System.Windows.Forms.GroupBox grpDocuments;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblSubmittedIndicator;
+        private System.Windows.Forms.Label lblMissingIndicator;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.GroupBox grpTimeline;
+        private System.Windows.Forms.DataGridView dgvTimeline;
     }
 }
--- ApplicantAuthDocumentManagement/Forms/MyDocumentForm.cs
+++ ApplicantAuthDocumentManagement/Forms/MyDocumentForm.cs
@@ -1,20 +1,192 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ApplicantAuthDocumentManagement.Forms
 {
     public partial class MyDocumentForm : Form
     {
+        private int currentApplicantID = Session.ApplicantID;
+
         public MyDocumentForm()
         {
             InitializeComponent();
+        }
+
+        private void MyDocumentForm_Load(object sender, EventArgs e)
+        {
+            LoadDocumentRequirements();
+            LoadStatusTimeline();
+        }
+
+        /// <summary>
+        /// Fetches the current required documents and displays them in the upper grid (dgvDocuments)
+        /// </summary>
+        private void LoadDocumentRequirements()
+        {
+            string selectQuery = @"
+                SELECT r.RequirementTypeID AS 'Type ID',
+                       r.TypeName AS 'Requirement Name', 
+                       IFNULL(d.SubmissionStatus, 'Missing') AS 'Submission Status',
+                       IFNULL(d.FilePath, 'No File Uploaded') AS 'File Path Location'
+                FROM RequirementTypes r
+                LEFT JOIN ApplicantDocuments d ON r.RequirementTypeID = d.RequirementTypeID 
+                                              AND d.ApplicationID = (SELECT ApplicationID FROM Applications WHERE ApplicantID = @AppID LIMIT 1)";
+
+
+            using (MySqlConnection conn = DBConnection.GetConnection())
+            {
+                using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@AppID", currentApplicantID);
+
+                    try
+                    {
+                        conn.Open();
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        dgvDocuments.DataSource = dt;
+
+                        if (dgvDocuments.Columns.Contains("Type ID"))
+                        {
+                            dgvDocuments.Columns["Type ID"].Visible = false;
+                        }
+
+                        UpdateSubmissionSummaryCounters();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading requirements: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fetches the historical log updates and populates the tracking grid (dgvTimeline)
+        /// </summary>
+        private void LoadStatusTimeline()
+        {
+            string queryTimeline = @"
+                SELECT NewStatus AS 'Stage Progress Status', 
+                       Remarks AS 'HR Operational Evaluation Remarks', 
+                       ChangedAt AS 'Update Timestamp Log'
+                FROM ApplicationStatusHistory
+                WHERE ApplicationID = (SELECT ApplicationID FROM Applications WHERE ApplicantID = @AppID LIMIT 1)
+                ORDER BY ChangedAt DESC";
+
+            using (MySqlConnection conn = DBConnection.GetConnection())
+            {
+                using (MySqlCommand cmd = new MySqlCommand(queryTimeline, conn))
+                {
+                    cmd.Parameters.AddWithValue("@AppID", currentApplicantID);
+
+                    try
+                    {
+                        conn.Open();
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        dgvTimeline.DataSource = dt;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading status log: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Button action that opens a file browser window and saves the file record path to MySQL
+        /// </summary>
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (dgvDocuments.CurrentRow == null)
+            {
+                MessageBox.Show("Please select a specific requirement row item from the list above first.", "Selection Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int requirementTypeID = Convert.ToInt32(dgvDocuments.CurrentRow.Cells["Type ID"].Value);
+            string documentNameLabel = dgvDocuments.CurrentRow.Cells["Requirement Name"].Value.ToString();
+
+            using (OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Filter = "Document Files (*.pdf;*.docx;*.jpg;*.png)|*.pdf;*.docx;*.jpg;*.png|All Files (*.*)|*.*";
+                fileDialog.Title = "Select File for " + documentNameLabel;
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string chosenFilePath = fileDialog.FileName;
+
+                    string standardFileName = Path.GetFileName(chosenFilePath);
+
+                    string saveQuery = @"
+                        INSERT INTO ApplicantDocuments (ApplicationID, RequirementTypeID, FilePath, FileName, SubmissionStatus) 
+                        VALUES ((SELECT ApplicationID FROM Applications WHERE ApplicantID = @AppID LIMIT 1), @ReqTypeID, @DocPath, @DocName, 'Submitted')
+                        ON DUPLICATE KEY UPDATE FilePath = @DocPath, FileName = @DocName, SubmissionStatus = 'Submitted'";
+
+                    using (MySqlConnection conn = DBConnection.GetConnection())
+                    {
+                        using (MySqlCommand cmd = new MySqlCommand(saveQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@AppID", currentApplicantID);
+                            cmd.Parameters.AddWithValue("@ReqTypeID", requirementTypeID);
+                            cmd.Parameters.AddWithValue("@DocPath", chosenFilePath);
+                            cmd.Parameters.AddWithValue("@DocName", standardFileName);
+
+                            try
+                            {
+                                conn.Open();
+                                cmd.ExecuteNonQuery();
+
+                                MessageBox.Show(documentNameLabel + " has been recorded successfully!", "Upload Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                LoadDocumentRequirements();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Critical Error during insertion: " + ex.Message, "Execution Aborted", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Simple structural loops that count status occurrences and updates your designer labels separately
+        /// </summary>
+        private void UpdateSubmissionSummaryCounters()
+        {
+            int submittedCount = 0;
+            int missingCount = 0;
+
+            foreach (DataGridViewRow row in dgvDocuments.Rows)
+            {
+                if (row.Cells["Submission Status"].Value != null)
+                {
+                    string cellStatusValue = row.Cells["Submission Status"].Value.ToString();
+
+                    if (cellStatusValue == "Submitted")
+                    {
+                        submittedCount++;
+                    }
+                    else if (cellStatusValue == "Missing")
+                    {
+                        missingCount++;
+                    }
+                }
+            }
+
+            lblSubmittedIndicator.Text = "Submitted: " + submittedCount.ToString() + " slots";
+            lblMissingIndicator.Text = "Missing: " + missingCount.ToString() + " slots";
         }
     }
 }
