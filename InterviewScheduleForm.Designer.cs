04f0650a1ae2346d3446d06e963c1b60b0d8ead8
ky-9 <kylamaee09@gmail.com>
2026-06-11 00:07:12 +0800

    Add files via upload

---
+++ InterviewScheduleForm.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace HRInterviewScheduleForm
+{
+    partial class InterviewScheduleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvSchedule = new System.Windows.Forms.DataGridView();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtInterviewer = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpScheduleDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtApplicant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).BeginInit();
+            this.Panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Thistle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1227, 113);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(478, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INTERVIEW SCHEDULE FORM";
+            // 
+            // dgvSchedule
+            // 
+            this.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSchedule.Location = new System.Drawing.Point(12, 174);
+            this.dgvSchedule.Name = "dgvSchedule";
+            this.dgvSchedule.RowHeadersWidth = 51;
+            this.dgvSchedule.RowTemplate.Height = 24;
+            this.dgvSchedule.Size = new System.Drawing.Size(642, 620);
+            this.dgvSchedule.TabIndex = 1;
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.Color.Thistle;
+            this.Panel2.Controls.Add(this.btnRefresh);
+            this.Panel2.Controls.Add(this.btnClear);
+            this.Panel2.Controls.Add(this.btnDelete);
+            this.Panel2.Controls.Add(this.btnUpdate);
+            this.Panel2.Controls.Add(this.btnSave);
+            this.Panel2.Controls.Add(this.txtNotes);
+            this.Panel2.Controls.Add(this.label8);
+            this.Panel2.Controls.Add(this.cmbStatus);
+            this.Panel2.Controls.Add(this.label7);
+            this.Panel2.Controls.Add(this.txtInterviewer);
+            this.Panel2.Controls.Add(this.label6);
+            this.Panel2.Controls.Add(this.txtTime);
+            this.Panel2.Controls.Add(this.label5);
+            this.Panel2.Controls.Add(this.dtpScheduleDate);
+            this.Panel2.Controls.Add(this.label4);
+            this.Panel2.Controls.Add(this.txtJob);
+            this.Panel2.Controls.Add(this.label3);
+            this.Panel2.Controls.Add(this.txtApplicant);
+            this.Panel2.Controls.Add(this.label2);
+            this.Panel2.Location = new System.Drawing.Point(705, 174);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(534, 620);
+            this.Panel2.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(269, 455);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(88, 31);
+            this.btnRefresh.TabIndex = 18;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(269, 509);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 31);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(177, 509);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 31);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(177, 455);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(88, 31);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(220, 550);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(88, 31);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(177, 286);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(229, 163);
+            this.txtNotes.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(38, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Notes";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Scheduled",
+            "Completed",
+            "Cancelled"});
+            this.cmbStatus.Location = new System.Drawing.Point(177, 236);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(180, 24);
+            this.cmbStatus.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Status";
+            // 
+            // txtInterviewer
+            // 
+            this.txtInterviewer.Location = new System.Drawing.Point(177, 197);
+            this.txtInterviewer.Name = "txtInterviewer";
+            this.txtInterviewer.Size = new System.Drawing.Size(180, 22);
+            this.txtInterviewer.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Interviewer";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(177, 157);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(180, 22);
+            this.txtTime.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Interview Time";
+            // 
+            // dtpScheduleDate
+            // 
+            this.dtpScheduleDate.Location = new System.Drawing.Point(177, 117);
+            this.dtpScheduleDate.Name = "dtpScheduleDate";
+            this.dtpScheduleDate.Size = new System.Drawing.Size(229, 22);
+            this.dtpScheduleDate.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Interview Date";
+            // 
+            // txtJob
+            // 
+            this.txtJob.Location = new System.Drawing.Point(177, 80);
+            this.txtJob.Name = "txtJob";
+            this.txtJob.ReadOnly = true;
+            this.txtJob.Size = new System.Drawing.Size(180, 22);
+            this.txtJob.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Job Title:";
+            // 
+            // txtApplicant
+            // 
+            this.txtApplicant.Location = new System.Drawing.Point(177, 47);
+            this.txtApplicant.Name = "txtApplicant";
+            this.txtApplicant.ReadOnly = true;
+            this.txtApplicant.Size = new System.Drawing.Size(180, 22);
+            this.txtApplicant.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Applicant Name:";
+            // 
+            // InterviewScheduleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1269, 832);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.dgvSchedule);
+            this.Controls.Add(this.panel1);
+            this.Name = "InterviewScheduleForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.InterviewScheduleForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).EndInit();
+            this.Panel2.ResumeLayout(false);
+            this.Panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSchedule;
+        private System.Windows.Forms.Panel Panel2;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApplicant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpScheduleDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInterviewer;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ InterviewScheduleForm.cs
@@ -0,0 +1,266 @@
+﻿using HRSystem;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace HRInterviewScheduleForm
+{
+    public partial class InterviewScheduleForm : Form
+    {
+        DBConnection db = new DBConnection();
+
+        int selectedScheduleID = 0;
+        int selectedApplicationID = 0;
+
+        public InterviewScheduleForm()
+        {
+            InitializeComponent();
+
+            this.Load += InterviewScheduleForm_Load;
+
+            dgvSchedule.CellClick += dgvSchedule_CellClick;
+
+            btnSave.Click += btnSave_Click;
+            btnUpdate.Click += btnUpdate_Click;
+            btnDelete.Click += btnDelete_Click;
+            btnRefresh.Click += btnRefresh_Click;
+            btnClear.Click += btnClear_Click;
+        }
+
+        // ================= LOAD =================
+        private void InterviewScheduleForm_Load(object sender, EventArgs e)
+        {
+            cmbStatus.Items.Clear();
+            cmbStatus.Items.Add("Scheduled");
+            cmbStatus.Items.Add("Completed");
+            cmbStatus.Items.Add("Cancelled");
+
+            LoadSchedules();
+        }
+
+        // ================= SAFE HELPERS =================
+        private string SafeString(object value)
+        {
+            return value == DBNull.Value || value == null ? "" : value.ToString();
+        }
+
+        private int SafeInt(object value)
+        {
+            return value == DBNull.Value || value == null ? 0 : Convert.ToInt32(value);
+        }
+
+        private DateTime SafeDate(object value)
+        {
+            return value == DBNull.Value || value == null ? DateTime.Now : Convert.ToDateTime(value);
+        }
+
+        // ================= LOAD DATA =================
+        private void LoadSchedules()
+        {
+            try
+            {
+                db.OpenConnection();
+
+                string query = @"
+                SELECT
+                    s.ScheduleID,
+                    s.ApplicationID,
+                    CONCAT(ap.FirstName,' ',ap.LastName) AS ApplicantName,
+                    j.JobTitle,
+                    s.ScheduleDate,
+                    s.ScheduleTime,
+                    s.Interviewer,
+                    s.Status,
+                    s.Notes
+                FROM InterviewSchedules s
+                INNER JOIN Applications a ON s.ApplicationID = a.ApplicationID
+                INNER JOIN Applicants ap ON a.ApplicantID = ap.ApplicantID
+                INNER JOIN JobVacancies j ON a.VacancyID = j.VacancyID";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, db.GetConnection());
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvSchedule.DataSource = dt;
+
+                db.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Load Error: " + ex.Message);
+            }
+        }
+
+        // ================= SELECT ROW =================
+        private void dgvSchedule_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            DataGridViewRow row = dgvSchedule.Rows[e.RowIndex];
+
+            selectedScheduleID = SafeInt(row.Cells["ScheduleID"].Value);
+            selectedApplicationID = SafeInt(row.Cells["ApplicationID"].Value);
+
+            txtApplicant.Text = SafeString(row.Cells["ApplicantName"].Value);
+            txtJob.Text = SafeString(row.Cells["JobTitle"].Value);
+
+            dtpScheduleDate.Value = SafeDate(row.Cells["ScheduleDate"].Value);
+
+            txtTime.Text = SafeString(row.Cells["ScheduleTime"].Value);
+            txtInterviewer.Text = SafeString(row.Cells["Interviewer"].Value);
+            cmbStatus.Text = SafeString(row.Cells["Status"].Value);
+            txtNotes.Text = SafeString(row.Cells["Notes"].Value);
+        }
+
+        // ================= SAVE =================
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedApplicationID == 0)
+                {
+                    MessageBox.Show("Please select an applicant.");
+                    return;
+                }
+
+                db.OpenConnection();
+
+                string insert = @"
+                INSERT INTO InterviewSchedules
+                (ApplicationID, ScheduleDate, ScheduleTime, Interviewer, Status, Notes)
+                VALUES
+                (@app,@date,@time,@interviewer,@status,@notes)";
+
+                MySqlCommand cmd = new MySqlCommand(insert, db.GetConnection());
+
+                cmd.Parameters.AddWithValue("@app", selectedApplicationID);
+                cmd.Parameters.AddWithValue("@date", dtpScheduleDate.Value.Date);
+                cmd.Parameters.AddWithValue("@time", txtTime.Text);
+                cmd.Parameters.AddWithValue("@interviewer", txtInterviewer.Text);
+                cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
+                cmd.Parameters.AddWithValue("@notes", txtNotes.Text);
+
+                cmd.ExecuteNonQuery();
+
+                db.CloseConnection();
+
+                MessageBox.Show("Interview Scheduled!");
+
+                LoadSchedules();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Save Error: " + ex.Message);
+            }
+        }
+
+        // ================= UPDATE =================
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedScheduleID == 0)
+                {
+                    MessageBox.Show("Select a schedule first.");
+                    return;
+                }
+
+                db.OpenConnection();
+
+                string update = @"
+                UPDATE InterviewSchedules
+                SET ScheduleDate=@date,
+                    ScheduleTime=@time,
+                    Interviewer=@interviewer,
+                    Status=@status,
+                    Notes=@notes
+                WHERE ScheduleID=@id";
+
+                MySqlCommand cmd = new MySqlCommand(update, db.GetConnection());
+
+                cmd.Parameters.AddWithValue("@id", selectedScheduleID);
+                cmd.Parameters.AddWithValue("@date", dtpScheduleDate.Value.Date);
+                cmd.Parameters.AddWithValue("@time", txtTime.Text);
+                cmd.Parameters.AddWithValue("@interviewer", txtInterviewer.Text);
+                cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
+                cmd.Parameters.AddWithValue("@notes", txtNotes.Text);
+
+                cmd.ExecuteNonQuery();
+
+                db.CloseConnection();
+
+                MessageBox.Show("Updated Successfully!");
+
+                LoadSchedules();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update Error: " + ex.Message);
+            }
+        }
+
+        // ================= DELETE =================
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedScheduleID == 0)
+                {
+                    MessageBox.Show("Select a schedule first.");
+                    return;
+                }
+
+                db.OpenConnection();
+
+                string delete = "DELETE FROM InterviewSchedules WHERE ScheduleID=@id";
+
+                MySqlCommand cmd = new MySqlCommand(delete, db.GetConnection());
+                cmd.Parameters.AddWithValue("@id", selectedScheduleID);
+
+                cmd.ExecuteNonQuery();
+
+                db.CloseConnection();
+
+                MessageBox.Show("Deleted Successfully!");
+
+                LoadSchedules();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Delete Error: " + ex.Message);
+            }
+        }
+
+        // ================= REFRESH =================
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadSchedules();
+        }
+
+        // ================= CLEAR =================
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            txtApplicant.Clear();
+            txtJob.Clear();
+            txtTime.Clear();
+            txtInterviewer.Clear();
+            txtNotes.Clear();
+
+            cmbStatus.SelectedIndex = -1;
+
+            selectedScheduleID = 0;
+            selectedApplicationID = 0;
+
+            dgvSchedule.ClearSelection();
+        }
+    }
+}
